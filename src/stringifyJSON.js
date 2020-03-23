064517194541ce0ebe6b5fe4f022b2e8b1d5d49c
mta630 <michael.t.albert89@gmail.com>
2020-03-23 17:48:27 -0500

    stringifyJSON.js completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,14 +5,10 @@
 
 var stringifyJSON = function(obj) {
   var str = '';
-
   var invalidArrList = ['undefined', 'function', 'symbol'];
+  var test = (x) => !invalidArrList.includes(typeof x);
 
-  if (invalidArrList.includes(typeof obj)) {
-    return 'null';
-  }
-
-  if (obj === null) {
+  if (invalidArrList.includes(typeof obj) || obj === null) {
     return 'null';
   }
 
@@ -20,42 +16,38 @@ var stringifyJSON = function(obj) {
     return `"${obj}"`;
   }
 
-  if (typeof obj === 'boolean') {
-    str += obj;
-  }
-
-  if (typeof obj === 'number') {
+  if (typeof obj === 'boolean' || typeof obj === 'number') {
     str += obj;
   }
 
 
   if (typeof obj === 'object' && !Array.isArray(obj)) {
 
-    if ()
     for (let key in obj) {
-      if () {
+      if (test(obj[key])) {
         str += stringifyJSON(key);
+        str += ':';
+        str += stringifyJSON(obj[key]);
       }
-      str += ':';
-      str += stringifyJSON(obj[key]);
 
-      if (key === Object.keys(obj).pop()){
+      if (key === Object.keys(obj).pop()) {
         break;
       }
 
-      str += ',';
+      if (test(obj[key])) {
+        str += ',';
+      }
 
     }
+
     return '{' + str + '}';
   }
 
   if (Array.isArray(obj)) {
     var arr = [];
-    for (let i = 0; i < obj.length; i ++) {
-      arr.push(stringifyJSON(obj[i]));
-    }
+    obj.forEach(item => arr.push(stringifyJSON(item)));
     return str += '[' + arr.join(',') + ']';
   }
 
   return str;
-}
+};
